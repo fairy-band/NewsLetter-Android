6b62461d74d1abe57f1a0e5b13e5510baabc741f
LEE YOU BIN <youbeene3409@naver.com>
2025-07-12 20:18:17 +0900

    [FEAT] 플러그인 추가

--- presentation/build.gradle.kts
+++ presentation/build.gradle.kts
@@ -49,4 +49,8 @@ dependencies {
     implementation(libs.androidx.ui.graphics)
     implementation(libs.androidx.ui.tooling.preview)
     implementation(libs.androidx.material3)
+
+    // third party
+    implementation(platform(libs.firebase.bom))
+    implementation(libs.firebase.analytics)
 }
