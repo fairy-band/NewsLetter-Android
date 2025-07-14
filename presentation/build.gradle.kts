b9949b08534677ce5e9b0445033278ed98448186
LEE YOU BIN <youbeene3409@naver.com>
2025-07-15 06:07:05 +0900

    [FEAT] presentation 모듈 의존성 수정

--- presentation/build.gradle.kts
+++ presentation/build.gradle.kts
@@ -42,11 +42,14 @@ dependencies {
 
     // androidx
     implementation(libs.androidx.core.ktx)
-    implementation(libs.androidx.lifecycle.runtime.ktx)
-    implementation(libs.androidx.activity.compose)
+    implementation(libs.androidx.lifecycle.runtime.ktx) // todo: used
     implementation(platform(libs.androidx.compose.bom))
-    implementation(libs.androidx.ui)
+    implementation(libs.androidx.ui) // todo: used
     implementation(libs.androidx.ui.graphics)
     implementation(libs.androidx.ui.tooling.preview)
-    implementation(libs.androidx.material3)
+    implementation(libs.androidx.material3) // todo: used
+    implementation(libs.androidx.navigation.compose)
+    implementation(libs.androidx.compose.saveable)
+    implementation(libs.androidx.lifecycle.runtimeCompose)
+    implementation(libs.androidx.lifecycle.viewModelCompose)
 }
