0c1f3bb9b7ba62cec3f7613695a89f5815fbef72
LEE YOU BIN <youbeene3409@naver.com>
2025-07-15 15:55:57 +0900

    [FEAT] presentation 그래들 정리

--- presentation/build.gradle.kts
+++ presentation/build.gradle.kts
@@ -3,6 +3,7 @@ plugins {
     alias(libs.plugins.kotlin.android)
     alias(libs.plugins.kotlin.compose)
     alias(libs.plugins.ksp)
+    alias(libs.plugins.jetbrains.kotlin.serialization)
 }
 
 android {
@@ -40,21 +41,27 @@ android {
 dependencies {
     // module
     implementation(project(":data"))
-
-    // androidx
+    
+    // core
     implementation(libs.androidx.core.ktx)
-    implementation(libs.androidx.lifecycle.runtime.ktx)
-    implementation(platform(libs.androidx.compose.bom))
-    implementation(libs.androidx.ui)
-    implementation(libs.androidx.ui.graphics)
-    implementation(libs.androidx.ui.tooling.preview)
-    implementation(libs.androidx.material3)
-    implementation(libs.androidx.navigation.compose)
-    implementation(libs.androidx.compose.saveable)
-    implementation(libs.androidx.lifecycle.runtimeCompose)
-    implementation(libs.androidx.lifecycle.viewModelCompose)
 
-    // koin
+    // compose
+    implementation(platform(libs.androidx.compose.bom))
+    implementation(libs.bundles.compose.ui)
+    implementation(libs.androidx.compose.saveable)
+
+    // lifecycle
+    implementation(libs.bundles.lifecycle.compose)
+
+    // navigation
+    implementation(libs.androidx.navigation3.runtime)
+    implementation(libs.androidx.navigation3.ui)
+    implementation(libs.androidx.lifecycle.viewmodel.navigation3)
+
+    // di
     implementation(libs.koin.annotations)
     ksp(libs.koin.ksp)
+
+    // serialization
+    implementation(libs.kotlinx.serialization.core)
 }
