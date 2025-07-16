cbd2ed8a633c60f63d5b2e8676a1eb8da3cd64c4
LEE YOU BIN <youbeene3409@naver.com>
2025-07-17 08:25:28 +0900

    [FEAT] Color 구현

--- presentation/src/main/java/com/nexters/knownknowns/presentation/ui/theme/Color.kt
+++ presentation/src/main/java/com/nexters/knownknowns/presentation/ui/theme/Color.kt
@@ -2,10 +2,56 @@ package com.nexters.knownknowns.presentation.ui.theme
 
 import androidx.compose.ui.graphics.Color
 
-val Purple80 = Color(0xFFD0BCFF)
-val PurpleGrey80 = Color(0xFFCCC2DC)
-val Pink80 = Color(0xFFEFB8C8)
+// Background
+val BackgroundBase = Color(0xFFF7F8FA)
+val BackgroundSurface = Color(0xFFFFFFFF)
+val BackgroundElevated = Color(0xFFFFFFFF)
+val BackgroundOnSurface = Color(0xFFF7F8FA)
+val BackgroundOnElevated = Color(0xFFF7F8FA)
+val BackgroundSurfaceInverse = Color(0xFF1B1D1F)
+val BackgroundDimmed = Color(0xB3000000)
 
-val Purple40 = Color(0xFF6650a4)
-val PurpleGrey40 = Color(0xFF625b71)
-val Pink40 = Color(0xFF7D5260)
+// Fill
+val FillPrimary = Color(0xFFE8EBF0)
+val FillSecondary = Color(0xFFF2F4F7)
+val FillTertiary = Color(0xFFF7F8FA)
+val FillDisabled = Color(0xFFF2F4F7)
+val FillPrimaryInverse = Color(0xFF121212)
+val FillSecondaryInverse = Color(0xFF4D545C)
+val FillWhite = Color(0xFFFFFFFF)
+
+// Border
+val BorderPrimary = Color(0xFFE8EBF0)
+val BorderSecondary = Color(0xFFD7DCE0)
+val BorderStrong = Color(0xFF121212)
+val BorderDisabled = Color(0xFFE8EBF0)
+val BorderActive = Color(0xFFA3ADB8)
+
+// Divider
+val Divider1px = Color(0xFFF2F4F7)
+val Divider1pxStrong = Color(0xFFE8EBF0)
+val Divider10px = Color(0xFFF2F4F7)
+
+// Text
+val TextStrong = Color(0xFF121212)
+val TextPrimary = Color(0xFF1B1D1F)
+val TextSecondary = Color(0xFF4D545C)
+val TextTertiary = Color(0xFF828E99)
+val TextDisabled = Color(0xFFA3ADB8)
+val TextStrongInverse = Color(0xFFFFFFFF)
+
+// Icon
+val IconStrong = Color(0xFF121212)
+val IconPrimary = Color(0xFF1B1D1F)
+val IconSecondary = Color(0xFF4D545C)
+val IconTertiary = Color(0xFF828E99)
+val IconDisabled = Color(0xFFA3ADB8)
+val IconStrongInverse = Color(0xFFFFFFFF)
+
+// State
+val StatePositivePrimary = Color(0xFF27C434)
+val StatePositiveBorder = Color(0xFFCAF0CD)
+val StatePositiveBackground = Color(0xFFEBFAEC)
+val StateNegativePrimary = Color(0xFFF03E3E)
+val StateNegativeBorder = Color(0xFFFDCACA)
+val StateNegativeBackground = Color(0xFFFDF0F0)
