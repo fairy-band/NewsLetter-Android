0161e853099e5b481eef51e773ec3a25b81e098e
LEE YOU BIN <youbeene3409@naver.com>
2025-07-17 10:05:13 +0900

    [FEAT] KnownKnownsTheme 구현

--- presentation/src/main/java/com/nexters/knownknowns/presentation/ui/theme/Theme.kt
+++ presentation/src/main/java/com/nexters/knownknowns/presentation/ui/theme/Theme.kt
@@ -1,24 +1,59 @@
 package com.nexters.knownknowns.presentation.ui.theme
 
 import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.lightColorScheme
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.CompositionLocalProvider
+import androidx.compose.runtime.ReadOnlyComposable
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.staticCompositionLocalOf
 
-private val LightColorScheme = lightColorScheme(
-    primary = TextPrimary,
-    secondary = TextSecondary,
-    tertiary = TextTertiary
-)
+private val LocalKnownKnownsColors = staticCompositionLocalOf<KnownKnownsColors> {
+    error("No KnownKnownsColors provided")
+}
+
+private val LocalKnownKnownsTypography = staticCompositionLocalOf<KnownKnownsTypography> {
+    error("No LocalKnownKnownsTypography provided")
+}
+
+object KnownKnownsTheme {
+    val colors: KnownKnownsColors
+        @Composable
+        @ReadOnlyComposable
+        get() = LocalKnownKnownsColors.current
+
+    val typography: KnownKnownsTypography
+        @Composable
+        @ReadOnlyComposable
+        get() = LocalKnownKnownsTypography.current
+}
+
+@Composable
+fun ProvideKnownKnownsColorsAndTypography(
+    colors: KnownKnownsColors,
+    typography: KnownKnownsTypography,
+    content: @Composable () -> Unit
+) {
+    val provideColors = remember { colors.copy() }
+    provideColors.update(colors)
+    val provideTypography = remember { typography.copy() }.apply { update(typography) }
+    CompositionLocalProvider(
+        LocalKnownKnownsColors provides provideColors,
+        LocalKnownKnownsTypography provides provideTypography,
+        content = content
+    )
+}
 
 @Composable
 fun KnownKnownsTheme(
     content: @Composable () -> Unit
 ) {
-    val colorScheme = LightColorScheme
+    val colors = KnownKnownsColors()
+    val typography = KnownKnownsTypography()
 
-    MaterialTheme(
-        colorScheme = colorScheme,
-        typography = Typography,
-        content = content
-    )
+    ProvideKnownKnownsColorsAndTypography(
+        colors,
+        typography
+    ) {
+        MaterialTheme(content = content)
+    }
 }
