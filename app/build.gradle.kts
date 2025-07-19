ff8e9087fac06124aa84f410b2d33438835cf56e
HamBP <sjy_9484@naver.com>
2025-07-19 20:22:27 +0900

    [CHORE] 액션이 트리거되는 조건을 수정했어요.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -30,8 +30,8 @@ android {
         applicationId = "com.nexters.knownknowns"
         minSdk = 28
         targetSdk = 36
-        versionCode = 1
-        versionName = "1.0"
+        versionCode = 2
+        versionName = "0.0.1"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
