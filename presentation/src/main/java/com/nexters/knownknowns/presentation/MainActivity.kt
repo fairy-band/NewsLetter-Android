91ee8204d06fd809543afc3042fb4d195802a9ce
HamBP <sjy_9484@naver.com>
2025-07-21 00:57:05 +0900

    [FEAT] NavController를 CompositionLocal로 주입했어용

--- presentation/src/main/java/com/nexters/knownknowns/presentation/MainActivity.kt
+++ presentation/src/main/java/com/nexters/knownknowns/presentation/MainActivity.kt
@@ -4,6 +4,8 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.runtime.CompositionLocalProvider
+import androidx.compose.runtime.staticCompositionLocalOf
 import androidx.navigation3.runtime.NavEntry
 import androidx.navigation3.runtime.NavKey
 import androidx.navigation3.runtime.entry
@@ -11,8 +13,13 @@ import androidx.navigation3.runtime.entryProvider
 import androidx.navigation3.ui.NavDisplay
 import com.nexters.knownknowns.core.theme.KnownKnownsTheme
 import com.nexters.knownknowns.presentation.feature.main.MainScreen
+import com.nexters.knownknowns.presentation.navigation.NavController
 import com.nexters.knownknowns.presentation.navigation.Screen
 import com.nexters.knownknowns.presentation.navigation.rememberNavController
+
+val LocalNavController = staticCompositionLocalOf<NavController> {
+    error("NavController가 주입되지 않았습니다.")
+}
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -22,11 +29,13 @@ class MainActivity : ComponentActivity() {
             KnownKnownsTheme {
                 val navController = rememberNavController()
 
-                NavDisplay(
-                    backStack = navController.backStack,
-                    onBack = { navController.pop() },
-                    entryProvider = createEntryProvider()
-                )
+                CompositionLocalProvider(LocalNavController provides navController) {
+                    NavDisplay(
+                        backStack = navController.backStack,
+                        onBack = { navController.pop() },
+                        entryProvider = createEntryProvider()
+                    )
+                }
             }
         }
     }
