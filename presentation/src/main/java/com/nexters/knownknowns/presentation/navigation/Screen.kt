d251c2e90b948f025119e1f09acab4778d0d9d2e
HamBP <sjy_9484@naver.com>
2025-07-21 20:09:22 +0900

    [FIX] 요상한 곳에서 어노테이션 붙였던 문제를 수정했어요.

--- presentation/src/main/java/com/nexters/knownknowns/presentation/navigation/Screen.kt
+++ presentation/src/main/java/com/nexters/knownknowns/presentation/navigation/Screen.kt
@@ -3,7 +3,8 @@ package com.nexters.knownknowns.presentation.navigation
 import androidx.navigation3.runtime.NavKey
 import kotlinx.serialization.Serializable
 
-@Serializable
 sealed interface Screen : NavKey {
+
+    @Serializable
     data object Main : Screen
 }
