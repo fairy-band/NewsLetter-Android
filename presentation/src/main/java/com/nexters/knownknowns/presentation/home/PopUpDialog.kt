054346db148056217474c5b0151b1e1bdc8a3bee
LEE YOU BIN <youbeene3409@naver.com>
2025-07-25 18:58:21 +0900

    [FEAT] 닫힘 버튼 UI

--- presentation/src/main/java/com/nexters/knownknowns/presentation/home/PopUpDialog.kt
+++ presentation/src/main/java/com/nexters/knownknowns/presentation/home/PopUpDialog.kt
@@ -1,21 +1,31 @@
 package com.nexters.knownknowns.presentation.home
 
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.pager.HorizontalPager
 import androidx.compose.foundation.pager.PageSize
 import androidx.compose.foundation.pager.rememberPagerState
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalConfiguration
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.window.Dialog
 import androidx.compose.ui.window.DialogProperties
+import com.nexters.knownknowns.core.theme.KnownKnownsTheme
+import com.nexters.knownknowns.presentation.R
 
 private data class CarouselItem(
     val id: Int,
@@ -118,6 +128,18 @@ internal fun PopUpDialog(
                     onClick = onDismissRequest,
                 )
             }
+            Spacer(modifier = Modifier.height(42.dp))
+            Image(
+                painter = painterResource(id = R.drawable.ic_popup_dismiss),
+                contentDescription = "pop up dismiss button",
+                modifier = Modifier
+                    .background(
+                        color = KnownKnownsTheme.colors.backgroundBase.copy(alpha = 0.1f),
+                        shape = CircleShape
+                    )
+                    .padding(13.dp)
+                    .align(Alignment.CenterHorizontally)
+            )
         }
     }
 }
