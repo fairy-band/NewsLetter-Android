068989ab72b530836f94bda5863ad5ce73d01505
LEE YOU BIN <youbeene3409@naver.com>
2025-07-28 16:47:28 +0900

    [CHORE] 불필요한 어노테이션 제거

--- core/src/main/java/com/nexters/knownknowns/core/extension/Modifier.kt
+++ core/src/main/java/com/nexters/knownknowns/core/extension/Modifier.kt
@@ -1,13 +1,11 @@
 package com.nexters.knownknowns.core.extension
 
-import android.annotation.SuppressLint
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.interaction.MutableInteractionSource
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.composed
 
-@SuppressLint("ModifierFactoryUnreferencedReceiver")
 inline fun Modifier.noRippleClickable(crossinline onClick: () -> Unit): Modifier = composed {
     clickable(indication = null,
         interactionSource = remember { MutableInteractionSource() }) {
