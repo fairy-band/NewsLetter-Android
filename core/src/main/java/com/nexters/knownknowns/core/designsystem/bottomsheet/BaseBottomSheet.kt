16656fa354962e5b60b22a8f58b02e6bbb91efe8
LEE YOU BIN <youbeene3409@naver.com>
2025-07-30 00:55:10 +0900

    [DESIGN] BottomSheet 디자인시스템 구축

---
+++ core/src/main/java/com/nexters/knownknowns/core/designsystem/bottomsheet/BaseBottomSheet.kt
@@ -0,0 +1,24 @@
+package com.nexters.knownknowns.core.designsystem.bottomsheet
+
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.ModalBottomSheet
+import androidx.compose.material3.SheetState
+import androidx.compose.material3.rememberModalBottomSheetState
+import androidx.compose.runtime.Composable
+import com.nexters.knownknowns.core.theme.KnownKnownsTheme
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun BaseBottomSheet(
+    onDismissRequest: () -> Unit,
+    sheetState: SheetState = rememberModalBottomSheetState(),
+    content: @Composable () -> Unit,
+) {
+    ModalBottomSheet(
+        onDismissRequest = onDismissRequest,
+        sheetState = sheetState,
+        containerColor = KnownKnownsTheme.colors.backgroundSurface,
+    ) {
+        content()
+    }
+}
