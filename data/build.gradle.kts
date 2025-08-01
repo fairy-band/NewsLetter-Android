e03394c1722d0e34f7d3b17e07a5070078c3dab6
LEE YOU BIN <youbeene3409@naver.com>
2025-08-01 19:56:48 +0900

    [FEAT] json 라이브러리 추가

--- data/build.gradle.kts
+++ data/build.gradle.kts
@@ -1,7 +1,14 @@
+import java.util.Properties
+
 plugins {
     alias(libs.plugins.android.library)
     alias(libs.plugins.kotlin.android)
     alias(libs.plugins.ksp)
+    alias(libs.plugins.kotlin.serialization)
+}
+
+val properties = Properties().apply {
+    load(rootProject.file("local.properties").inputStream())
 }
 
 android {
@@ -16,6 +23,13 @@ android {
     }
 
     buildTypes {
+        debug {
+            buildConfigField(
+                "String",
+                "BASE_URL",
+                properties.getProperty("base.url")
+            )
+        }
         release {
             isMinifyEnabled = false
             proguardFiles(
@@ -24,16 +38,25 @@ android {
             )
         }
     }
+
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_11
         targetCompatibility = JavaVersion.VERSION_11
     }
+
     kotlinOptions {
         jvmTarget = "11"
+    }
+
+    buildFeatures {
+        buildConfig = true
     }
 }
 
 dependencies {
+    // module
+    implementation(project(":core"))
+
     // network
     implementation(libs.bundles.retrofit)
     implementation(libs.okhttp.logging)
@@ -57,4 +80,8 @@ dependencies {
     // data store
     implementation(libs.androidx.datastore.preferences)
     implementation(libs.androidx.datastore.preferences.core)
+
+    // serialization
+    implementation(libs.kotlinx.serialization.core)
+    implementation(libs.kotlinx.serialization.json)
 }
