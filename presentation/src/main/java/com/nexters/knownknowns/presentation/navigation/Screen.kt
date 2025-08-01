164f600cedcf5f17923b8660ace29c503d0d2a95
HamBP <sjy_9484@naver.com>
2025-08-02 01:34:30 +0900

    [LOG] 캐로셀 화면 체류 시간을 추적해요.

--- presentation/src/main/java/com/nexters/knownknowns/presentation/navigation/Screen.kt
+++ presentation/src/main/java/com/nexters/knownknowns/presentation/navigation/Screen.kt
@@ -21,4 +21,9 @@ sealed class Screen(val name: String) : NavKey {
     data class WebView(
         val url: String,
     ) : Screen("webview")
+
+    // 실제 화면은 아니지만, GA 로그를 찍기 위해 관리하는 객체들. 그러나 나중에 백스택으로 관리 가능할지 검토할 수 있음.
+    data object NewsLetterCarousel : Screen("newsletter_carousel")
+    data object BottomSheetNotification : Screen("bottom_sheet_notification")
+    data object BottomSheetCustom : Screen("bottom_sheet_custom")
 }
