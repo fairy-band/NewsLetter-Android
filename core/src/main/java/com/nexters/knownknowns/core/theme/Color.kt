10b1e6e1ffb98a8d88bed232087c115472c26e90
HamBP <sjy_9484@naver.com>
2025-08-02 22:24:00 +0900

    [FEAT] 고정 시드 값을 주고, text color를 생성하는 로직도 작성했어요.

--- core/src/main/java/com/nexters/knownknowns/core/theme/Color.kt
+++ core/src/main/java/com/nexters/knownknowns/core/theme/Color.kt
@@ -1,6 +1,8 @@
 package com.nexters.knownknowns.core.theme
 
 import androidx.compose.ui.graphics.Color
+import java.time.LocalDate
+import kotlin.random.Random
 
 /**
  * [ColorPalette]
@@ -231,8 +233,15 @@ val aColorSet = listOf(
     listOf(ColorPalette.lemonYellow300, ColorPalette.lemonYellow200, ColorPalette.lemonYellow400),
 )
 
+/**
+ * 같은 날이면 동일한 seed 값을 이용하여 여러 번 호출해도 동일한 결과가 나온다.
+ * 따라서 오늘의 색깔을 별도로 저장하거나 전달할 필요가 없다.
+ */
 fun generateCardColorA(keywords: List<String>): List<Color> {
-    val randomColorSet = aColorSet.shuffled()
+    val today = LocalDate.now()
+    val seed = today.year * 10000 + today.monthValue * 100 + today.dayOfMonth
+
+    val randomColorSet = aColorSet.shuffled(Random(seed))
     val keywordNumberMap = mutableMapOf<String, Int>()
     var cnt = 0
     for (keyword in keywords) {
@@ -244,5 +253,37 @@ fun generateCardColorA(keywords: List<String>): List<Color> {
     return keywords.mapIndexed { index, keyword ->
         val keywordNumber = keywordNumberMap[keyword] ?: 5
         randomColorSet[keywordNumber][index % 3]
+    }
+}
+
+val cardTextColorsB = listOf(
+    ColorPalette.blue300,
+    ColorPalette.orange400,
+    ColorPalette.pink300,
+    ColorPalette.purple200,
+    ColorPalette.green300,
+    ColorPalette.lemonYellow300,
+)
+
+/**
+ * 같은 날이면 동일한 seed 값을 이용하여 여러 번 호출해도 동일한 결과가 나온다.
+ * 따라서 오늘의 색깔을 별도로 저장하거나 전달할 필요가 없다.
+ */
+fun generateCardTextColorA(keywords: List<String>): List<Color> {
+    val today = LocalDate.now()
+    val seed = today.year * 10000 + today.monthValue * 100 + today.dayOfMonth
+
+    val randomColorSet = cardTextColorsB.shuffled(Random(seed))
+    val keywordNumberMap = mutableMapOf<String, Int>()
+    var cnt = 0
+    for (keyword in keywords) {
+        if (keywordNumberMap[keyword] == null) {
+            keywordNumberMap[keyword] = cnt++
+        }
+    }
+
+    return keywords.map { keyword ->
+        val keywordNumber = keywordNumberMap[keyword] ?: 5
+        randomColorSet[keywordNumber]
     }
 }
