69a3880ff1b03a17d74c320d2217bff7bb2056a3
LEE YOU BIN <youbeene3409@naver.com>
2025-08-05 00:45:42 +0900

    [CHORE] userId를 가져와 조합하는 위치를 레포지토리로 변경

--- data/src/main/java/com/nexters/knownknowns/data/repositoryimpl/UserRepositoryImpl.kt
+++ data/src/main/java/com/nexters/knownknowns/data/repositoryimpl/UserRepositoryImpl.kt
@@ -1,9 +1,8 @@
 package com.nexters.knownknowns.data.repositoryimpl
 
+import com.nexters.knownknowns.data.datasource.AuthDataSource
 import com.nexters.knownknowns.data.datasource.UserDataSource
-import com.nexters.knownknowns.data.local.auth.AuthDataStore
 import com.nexters.knownknowns.data.local.user.ClickState
-import com.nexters.knownknowns.data.local.user.UserDataStore
 import com.nexters.knownknowns.data.model.request.UserInfoRequest
 import com.nexters.knownknowns.data.repository.UserRepository
 import kotlinx.coroutines.flow.Flow
@@ -12,27 +11,25 @@ import org.koin.core.annotation.Single
 
 @Single
 internal class UserRepositoryImpl(
-    private val userDataStore: UserDataStore,
+    private val authDataSource: AuthDataSource,
     private val userDataSource: UserDataSource,
-    private val authDataStore: AuthDataStore
 ) : UserRepository {
-    override val clickStateFlow: Flow<ClickState>
-        get() = userDataStore.clickStateFlow
+    override val clickStateFlow: Flow<ClickState> = userDataSource.clickStateFlow
 
     override suspend fun incrementClickCount() {
-        userDataStore.incrementClickCount()
+        userDataSource.incrementClickCount()
     }
 
     override suspend fun resetClickState() {
-        userDataStore.resetClickState()
+        userDataSource.resetClickState()
     }
 
     override suspend fun recordBottomSheetShown() {
-        userDataStore.recordBottomSheetShown()
+        userDataSource.recordBottomSheetShown()
     }
 
     override suspend fun putUserInfo(request: UserInfoRequest): Result<Unit> = runCatching {
-        val userId = authDataStore.userId.first()
+        val userId = authDataSource.getUserId().first()
 
         userDataSource.putUserInfo(
             userId = userId ?: 0L,
