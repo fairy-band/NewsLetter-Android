3811be53ae85916c68ab12e31ee96861f4629457
LEE YOU BIN <youbeene3409@naver.com>
2025-08-05 00:03:09 +0900

    [CHORE] 맞춤정보 API userId 하드코딩값 제거

--- data/src/main/java/com/nexters/knownknowns/data/datasource/UserDataSource.kt
+++ data/src/main/java/com/nexters/knownknowns/data/datasource/UserDataSource.kt
@@ -1,7 +1,19 @@
 package com.nexters.knownknowns.data.datasource
 
 import com.nexters.knownknowns.data.model.request.UserInfoRequest
+import com.nexters.knownknowns.data.remote.service.UserService
 
-internal interface UserDataSource {
-    suspend fun putUserInfo(request: UserInfoRequest)
+internal class UserDataSource(
+    private val userService: UserService,
+) {
+    suspend fun putUserInfo(
+        userId: Long,
+        request: UserInfoRequest
+    ) {
+
+        userService.putUserInfo(
+            userId = userId,
+            body = request
+        )
+    }
 }
--- data/src/main/java/com/nexters/knownknowns/data/repositoryimpl/UserRepositoryImpl.kt
+++ data/src/main/java/com/nexters/knownknowns/data/repositoryimpl/UserRepositoryImpl.kt
@@ -1,17 +1,20 @@
 package com.nexters.knownknowns.data.repositoryimpl
 
 import com.nexters.knownknowns.data.datasource.UserDataSource
+import com.nexters.knownknowns.data.local.auth.AuthDataStore
 import com.nexters.knownknowns.data.local.user.ClickState
 import com.nexters.knownknowns.data.local.user.UserDataStore
 import com.nexters.knownknowns.data.model.request.UserInfoRequest
 import com.nexters.knownknowns.data.repository.UserRepository
 import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.first
 import org.koin.core.annotation.Single
 
 @Single
 internal class UserRepositoryImpl(
     private val userDataStore: UserDataStore,
-    private val userDataSource: UserDataSource
+    private val userDataSource: UserDataSource,
+    private val authDataStore: AuthDataStore
 ) : UserRepository {
     override val clickStateFlow: Flow<ClickState>
         get() = userDataStore.clickStateFlow
@@ -29,6 +32,11 @@ internal class UserRepositoryImpl(
     }
 
     override suspend fun putUserInfo(request: UserInfoRequest): Result<Unit> = runCatching {
-            userDataSource.putUserInfo(request)
+        val userId = authDataStore.userId.first()
+
+        userDataSource.putUserInfo(
+            userId = userId ?: 0L,
+            request = request
+        )
     }
 }
