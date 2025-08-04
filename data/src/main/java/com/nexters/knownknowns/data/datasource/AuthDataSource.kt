8938e64b7d07dc10893704482f051e39d0d0d8e5
LEE YOU BIN <youbeene3409@naver.com>
2025-08-04 18:01:35 +0900

    [FEAT] AuthDataSource 구현

---
+++ data/src/main/java/com/nexters/knownknowns/data/datasource/AuthDataSource.kt
@@ -0,0 +1,25 @@
+package com.nexters.knownknowns.data.datasource
+
+import com.nexters.knownknowns.data.local.auth.AuthDataStore
+import com.nexters.knownknowns.data.model.request.RegisterRequest
+import com.nexters.knownknowns.data.model.response.RegisterResponse
+import com.nexters.knownknowns.data.remote.service.AuthService
+import kotlinx.coroutines.flow.Flow
+import org.koin.core.annotation.Single
+
+@Single
+class AuthDataSource(
+    private val authDataStore: AuthDataStore,
+    private val authService: AuthService,
+) {
+    fun getUserId(): Flow<Long?> = authDataStore.userId
+
+    suspend fun setUserId(id: Long) {
+        authDataStore.setUserId(id)
+    }
+
+    suspend fun registerUser(request: RegisterRequest): RegisterResponse =
+        authService.registerUser(
+            body = request
+        )
+}
