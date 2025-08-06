cf637aa6017f30b2ef6fe12b9fbbf6e7d43244c1
HamBP <sjy_9484@naver.com>
2025-08-06 20:32:59 +0900

    [CHORE] 테스트를 위해 버전 코드를 올렸어요.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -31,7 +31,7 @@ android {
         applicationId = "com.fairyband.soak"
         minSdk = 28
         targetSdk = 36
-        versionCode = 2
+        versionCode = 3
         versionName = "0.1.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
