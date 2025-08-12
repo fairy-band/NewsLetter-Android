39eaea18c0d284d4e3f1eb90623610e221a05894
HamBP <sjy_9484@naver.com>
2025-08-12 21:17:21 +0900

    [FEAT] 연속 일수와 방문 날짜를 추적할 수 있는 데이터 소스를 만들었어요.

--- data/src/main/java/com/fairyband/soak/data/datasource/UserDataSource.kt
+++ data/src/main/java/com/fairyband/soak/data/datasource/UserDataSource.kt
@@ -6,12 +6,22 @@ import com.fairyband.soak.data.model.request.UserInfoRequest
 import com.fairyband.soak.data.remote.service.UserService
 import kotlinx.coroutines.flow.Flow
 import org.koin.core.annotation.Single
+import java.time.LocalDate
 
 @Single
 internal class UserDataSource(
     private val userDataStore: UserDataStore,
     private val userService: UserService,
 ) {
+    val clickStateFlow: Flow<ClickState>
+        get() = userDataStore.clickStateFlow
+
+    val streak: Flow<Int>
+        get() = userDataStore.streakFlow
+
+    val notificationSettingDateFlow: Flow<LocalDate>
+        get() = userDataStore.notificationSettingDateFlow
+
     suspend fun putUserInfo(
         userId: Long,
         request: UserInfoRequest
@@ -21,9 +31,6 @@ internal class UserDataSource(
             body = request
         )
     }
-
-    val clickStateFlow: Flow<ClickState>
-        get() = userDataStore.clickStateFlow
 
     suspend fun incrementClickCount() {
         userDataStore.incrementClickCount()
@@ -35,5 +42,9 @@ internal class UserDataSource(
 
     suspend fun recordBottomSheetShown() {
         userDataStore.recordBottomSheetShown()
+    }
+
+    suspend fun visitApp() {
+        userDataStore.updateLastVisitedDate()
     }
 }
