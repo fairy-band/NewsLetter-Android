7f9c398078bc1127a929efe12d3b4c3044abbd76
LEE YOU BIN <youbeene3409@naver.com>
2025-08-16 03:59:25 +0900

    [DESIGN] string 값 추출

---
+++ presentation/src/main/java/com/fairyband/soak/presentation/feature/setting/SettingScreen.kt
@@ -0,0 +1,46 @@
+package com.fairyband.soak.presentation.feature.setting
+
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.vector.ImageVector
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.res.vectorResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.fairyband.soak.core.theme.SoakTheme
+import com.fairyband.soak.presentation.R
+
+@Composable
+internal fun SettingScreen() {
+    Column(
+        modifier = Modifier.fillMaxWidth()
+    ) {
+        Image(
+            imageVector = ImageVector.vectorResource(id = R.drawable.ic_setting_leading),
+            contentDescription = "back button",
+            modifier = Modifier.padding(start = 6.dp)
+        )
+        Spacer(modifier = Modifier.height(16.dp))
+        Text(
+            text = stringResource(id = R.string.setting_info_title),
+            style = SoakTheme.typography.body16.copy(fontWeight = FontWeight.Medium),
+            color = SoakTheme.colors.textTertiary,
+        )
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+private fun SettingScreenPreview() {
+    SoakTheme {
+        SettingScreen()
+    }
+}
