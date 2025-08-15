bd9a03cbc7a68289cd9d61ee8601efd3b1d04016
LEE YOU BIN <youbeene3409@naver.com>
2025-08-16 05:31:21 +0900

    [DESIGN] 설정 화면 UI

--- presentation/src/main/java/com/fairyband/soak/presentation/feature/setting/SettingScreen.kt
+++ presentation/src/main/java/com/fairyband/soak/presentation/feature/setting/SettingScreen.kt
@@ -8,6 +8,7 @@ import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.HorizontalDivider
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
@@ -31,22 +32,79 @@ internal fun SettingScreen() {
             contentDescription = "back button",
             modifier = Modifier.padding(start = 6.dp)
         )
-        Spacer(modifier = Modifier.height(16.dp))
-        Text(
-            text = stringResource(id = R.string.setting_info_title),
-            style = SoakTheme.typography.body16.copy(fontWeight = FontWeight.Medium),
-            color = SoakTheme.colors.textTertiary,
-        )
-        SettingSection(title = stringResource(R.string.setting_info_title))
+        Column(
+            modifier = Modifier.padding(horizontal = 16.dp)
+        ) {
+            Spacer(modifier = Modifier.height(16.dp))
+            Text(
+                text = stringResource(id = R.string.setting_info_title),
+                style = SoakTheme.typography.body16.copy(fontWeight = FontWeight.Medium),
+                color = SoakTheme.colors.textTertiary,
+            )
+            Spacer(modifier = Modifier.height(24.dp))
+            SettingArrow(title = stringResource(R.string.setting_info_title))
+            Spacer(modifier = Modifier.height(24.dp))
+            SettingArrow(title = stringResource(R.string.setting_info_user))
+            Spacer(modifier = Modifier.height(24.dp))
+            SettingArrow(title = stringResource(R.string.setting_info_alarm))
+            Spacer(modifier = Modifier.height(24.dp))
+            HorizontalDivider(
+                color = SoakTheme.colors.borderPrimary,
+                modifier = Modifier.padding(horizontal = 0.5.dp)
+            )
+            Spacer(modifier = Modifier.height(24.dp))
+            Text(
+                text = stringResource(id = R.string.setting_version_title),
+                style = SoakTheme.typography.body16.copy(fontWeight = FontWeight.Medium),
+                color = SoakTheme.colors.textTertiary,
+            )
+            Spacer(modifier = Modifier.height(24.dp))
+            SettingText(
+                title = stringResource(R.string.setting_version_current),
+                subText = "1.0.1.329"
+            )
+            Spacer(modifier = Modifier.height(24.dp))
+            HorizontalDivider(
+                color = SoakTheme.colors.borderPrimary,
+                modifier = Modifier.padding(horizontal = 0.5.dp)
+            )
+            Spacer(modifier = Modifier.height(24.dp))
+            Text(
+                text = stringResource(id = R.string.setting_support_title),
+                style = SoakTheme.typography.body16.copy(fontWeight = FontWeight.Medium),
+                color = SoakTheme.colors.textTertiary,
+            )
+            Spacer(modifier = Modifier.height(24.dp))
+            SettingText(
+                title = stringResource(R.string.setting_support_contact),
+                subText = stringResource(R.string.setting_support_email)
+            )
+            Spacer(modifier = Modifier.height(24.dp))
+            HorizontalDivider(
+                color = SoakTheme.colors.borderPrimary,
+                modifier = Modifier.padding(horizontal = 0.5.dp)
+            )
+            Spacer(modifier = Modifier.height(24.dp))
+            Text(
+                text = stringResource(id = R.string.setting_policy_title),
+                style = SoakTheme.typography.body16.copy(fontWeight = FontWeight.Medium),
+                color = SoakTheme.colors.textTertiary,
+            )
+            Spacer(modifier = Modifier.height(24.dp))
+            SettingArrow(title = stringResource(R.string.setting_policy_service))
+            Spacer(modifier = Modifier.height(24.dp))
+            SettingArrow(title = stringResource(R.string.setting_policy_personal))
+        }
     }
 }
 
 @Composable
-private fun SettingSection(
+private fun SettingArrow(
     title: String,
+    modifier: Modifier = Modifier,
 ) {
     Row(
-        modifier = Modifier.fillMaxWidth(),
+        modifier = modifier.fillMaxWidth(),
         verticalAlignment = Alignment.CenterVertically,
         horizontalArrangement = Arrangement.SpaceBetween,
     ) {
@@ -57,6 +115,28 @@ private fun SettingSection(
         Image(
             imageVector = ImageVector.vectorResource(id = R.drawable.ic_setting_arrow),
             contentDescription = "arrow button"
+        )
+    }
+}
+
+@Composable
+private fun SettingText(
+    title: String,
+    subText: String,
+    modifier: Modifier = Modifier,
+) {
+    Row(
+        modifier = modifier.fillMaxWidth(),
+        verticalAlignment = Alignment.CenterVertically,
+        horizontalArrangement = Arrangement.SpaceBetween,
+    ) {
+        Text(
+            text = title,
+            style = SoakTheme.typography.body18.copy(fontWeight = FontWeight.Medium)
+        )
+        Text(
+            text = subText,
+            style = SoakTheme.typography.body16.copy(fontWeight = FontWeight.Normal)
         )
     }
 }
