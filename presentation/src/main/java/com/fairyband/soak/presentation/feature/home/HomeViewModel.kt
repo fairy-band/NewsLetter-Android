02127de015a844d43efccfd5b9e2f05edec0ed79
HamBP <sjy_9484@naver.com>
2025-08-23 00:20:53 +0900

    [REFACTOR] fetchNews 함수를 호출하는 방식으로 변경했어요.

--- presentation/src/main/java/com/fairyband/soak/presentation/feature/home/HomeViewModel.kt
+++ presentation/src/main/java/com/fairyband/soak/presentation/feature/home/HomeViewModel.kt
@@ -23,18 +23,19 @@ import kotlinx.coroutines.flow.asStateFlow
 import kotlinx.coroutines.flow.catch
 import kotlinx.coroutines.flow.combine
 import kotlinx.coroutines.flow.filter
+import kotlinx.coroutines.flow.firstOrNull
 import kotlinx.coroutines.flow.launchIn
 import kotlinx.coroutines.flow.map
+import kotlinx.coroutines.flow.onEach
 import kotlinx.coroutines.flow.stateIn
 import kotlinx.coroutines.flow.update
 import kotlinx.coroutines.launch
 import org.koin.android.annotation.KoinViewModel
 import timber.log.Timber
-import java.time.LocalDate
 
 @KoinViewModel
 class HomeViewModel(
-    newsRepository: NewsRepository,
+    private val newsRepository: NewsRepository,
     private val authRepository: AuthRepository,
     private val userRepository: UserRepository,
     private val bottomSheetUseCase: BottomSheetUseCase,
@@ -62,18 +63,8 @@ class HomeViewModel(
             false
         )
 
-    val news: StateFlow<ImmutableList<NewsFeed>> = newsRepository
-        .getNews()
-        .map {
-            it.map { response ->
-                response.toNewsFeed()
-            }.toImmutableList()
-        }
-        .stateIn(
-            viewModelScope,
-            SharingStarted.WhileSubscribed(5_000),
-            persistentListOf()
-        )
+    private val _news = MutableStateFlow<ImmutableList<NewsFeed>>(persistentListOf())
+    val news = _news.asStateFlow()
 
     val cardColorType: StateFlow<String> = remoteConfigRepository
         .getCardColorType()
@@ -84,8 +75,19 @@ class HomeViewModel(
         )
 
     init {
+        fetchNews()
         visitApp()
         observeBottomSheetTrigger()
+    }
+
+    fun fetchNews() {
+        newsRepository.getNews()
+            .onEach { response ->
+                _news.update {
+                    response.map { it.toNewsFeed() }.toImmutableList()
+                }
+            }
+            .launchIn(viewModelScope)
     }
 
     private fun observeBottomSheetTrigger() {
