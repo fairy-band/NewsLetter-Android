351477f474fc4ad0017a4621408c6138cfbb987c
LEE YOU BIN <youbeene3409@naver.com>
2025-10-03 02:19:35 +0900

    [FEAT] NavController 복구

--- presentation/src/main/java/com/fairyband/soak/presentation/navigation/NavController.kt
+++ presentation/src/main/java/com/fairyband/soak/presentation/navigation/NavController.kt
@@ -1,10 +1,17 @@
 package com.fairyband.soak.presentation.navigation
 
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.remember
+import androidx.compose.runtime.snapshotFlow
 import androidx.navigation3.runtime.NavBackStack
 import androidx.navigation3.runtime.NavKey
 import androidx.navigation3.runtime.rememberNavBackStack
+import com.google.firebase.Firebase
+import com.google.firebase.analytics.FirebaseAnalytics
+import com.google.firebase.analytics.analytics
+import com.google.firebase.analytics.logEvent
+import kotlinx.coroutines.flow.filterNotNull
 
 class NavController(val backStack: NavBackStack) {
 
@@ -25,6 +32,26 @@ class NavController(val backStack: NavBackStack) {
 @Composable
 fun rememberNavController(): NavController {
     val backStack = rememberNavBackStack(Screen.Splash)
+
+    LaunchedEffect(backStack) {
+        val analytics = Firebase.analytics
+        var previousScreenName = "none"
+
+        snapshotFlow { backStack.lastOrNull() }
+            .filterNotNull()
+            .collect {
+                val screenName = (it as? Screen)?.name ?: "Unknown"
+
+                analytics.logEvent("page_view") {
+                    param(FirebaseAnalytics.Param.SCREEN_NAME, screenName)
+                    param("platform", "Android")
+                    param("previous_screen", previousScreenName)
+                    param("arguments", it.toString())
+                }
+
+                previousScreenName = screenName
+            }
+    }
 
     return remember { NavController(backStack) }
 }
