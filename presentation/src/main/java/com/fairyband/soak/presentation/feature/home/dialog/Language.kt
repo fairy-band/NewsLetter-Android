bac614e3f667b85ca50a4243b5f344d82421db66
LEE YOU BIN <youbeene3409@naver.com>
2025-11-18 19:00:54 +0900

    [FEAT] UNKNOWN 문자열 변경

--- presentation/src/main/java/com/fairyband/soak/presentation/feature/home/dialog/Language.kt
+++ presentation/src/main/java/com/fairyband/soak/presentation/feature/home/dialog/Language.kt
@@ -8,7 +8,7 @@ enum class Language(
 ) {
     KOREAN("KOREAN", R.string.home_popup_language_korean),
     ENGLISH("ENGLISH", R.string.home_popup_language_english),
-    UNKNOWN("UNKNOWN", R.string.home_popup_language_korean);
+    UNKNOWN("UNKNOWN", R.string.home_popup_language_unknown);
 
     companion object {
         fun fromKey(key: String): Language = entries.find { it.key == key } ?: UNKNOWN
