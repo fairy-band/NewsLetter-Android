aefaaf566a16e44aec64a0cc03adeebceae2bc68
HamBP <sjy_9484@naver.com>
2025-11-18 21:56:26 +0900

    [FEAT] 임시로 사용할 다이얼로그를 만들었어요.

---
+++ core/src/main/java/com/fairyband/soak/core/designsystem/dialog/BaseDialog.kt
@@ -0,0 +1,45 @@
+package com.fairyband.soak.core.designsystem.dialog
+
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.ColumnScope
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Card
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.window.Dialog
+import androidx.compose.ui.window.DialogProperties
+
+@Composable
+fun BaseDialog(
+    modifier: Modifier = Modifier,
+    onDismiss: () -> Unit = {},
+    horizontalAlignment: Alignment.Horizontal = Alignment.CenterHorizontally,
+    content: @Composable ColumnScope.() -> Unit,
+) {
+    Dialog(
+        properties = DialogProperties(
+            dismissOnBackPress = true,
+            dismissOnClickOutside = true,
+            usePlatformDefaultWidth = false,
+        ),
+        onDismissRequest = onDismiss
+    ) {
+        Card(
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(horizontal = 32.dp),
+            shape = RoundedCornerShape(8.dp),
+        ) {
+            Column(
+                modifier = modifier.padding(20.dp),
+                horizontalAlignment = horizontalAlignment,
+            ) {
+                content()
+            }
+        }
+    }
+}
