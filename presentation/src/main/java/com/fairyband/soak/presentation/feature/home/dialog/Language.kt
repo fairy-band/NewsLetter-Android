bbe3b8dd16d270cc361fef80144a09f23d751727
LEE YOU BIN <youbeene3409@naver.com>
2025-11-18 18:54:12 +0900

    [FEAT] 이넘 클래스 작성

---
+++ presentation/src/main/java/com/fairyband/soak/presentation/feature/home/dialog/Language.kt
@@ -0,0 +1,16 @@
+package com.fairyband.soak.presentation.feature.home.dialog
+
+import com.fairyband.soak.presentation.R
+
+enum class Language(
+    val key: String,
+    val label: Int,
+) {
+    KOREAN("KOREAN", R.string.home_popup_language_korean),
+    ENGLISH("ENGLISH", R.string.home_popup_language_english),
+    UNKNOWN("UNKNOWN", R.string.home_popup_language_korean);
+
+    companion object {
+        fun fromKey(key: String): Language = entries.find { it.key == key } ?: UNKNOWN
+    }
+}
