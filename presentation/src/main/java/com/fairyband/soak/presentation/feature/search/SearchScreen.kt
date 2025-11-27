9ee87437ad03bbbda89c2c62ccdccf42c19cf7f3
HamBP <sjy_9484@naver.com>
2025-11-27 18:08:03 +0900

    [FEAT] 탐색 탭의 LazyGrid와 카드를 만들었어요.

---
+++ presentation/src/main/java/com/fairyband/soak/presentation/feature/search/SearchScreen.kt
@@ -0,0 +1,84 @@
+package com.fairyband.soak.presentation.feature.search
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.grid.GridCells
+import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Text
+import androidx.compose.material3.VerticalDivider
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.dp
+import com.fairyband.soak.core.theme.SoakColors
+import com.fairyband.soak.core.theme.SoakTheme
+
+@Composable
+fun SearchScreen() {
+    Column(
+        modifier = Modifier.padding(horizontal = 16.dp)
+    ) {
+        Text("전체 (100)")
+        LazyVerticalGrid(
+            columns = GridCells.Fixed(2),
+            verticalArrangement = Arrangement.spacedBy(8.dp),
+            horizontalArrangement = Arrangement.spacedBy(8.dp),
+        ) {
+            items(14) {
+                Card()
+            }
+        }
+    }
+}
+
+@Composable
+private fun Card() {
+    val soakColors = remember { SoakColors() }
+
+    Column(
+        modifier = Modifier
+            .height(176.dp)
+            .background(
+                shape = RoundedCornerShape(16.dp),
+                color = soakColors.lemonYellowBackgroundPrimary
+            )
+            .padding(16.dp)
+    ) {
+        Text(
+            "네온, PostgreSQL 전문가도 놓친 치명적 실수",
+            style = SoakTheme.typography.body15.copy(color = SoakTheme.colors.textStrong)
+        )
+        Spacer(modifier = Modifier.weight(1f))
+        Row(
+            modifier = Modifier.height(18.dp)
+        ) {
+            Text(
+                "Kotlin", style = SoakTheme.typography.body13.copy(
+                    color = Color(0x80121212),
+                    fontWeight = FontWeight.Bold,
+                )
+            )
+            VerticalDivider(
+                modifier = Modifier.padding(
+                    vertical = 2.dp,
+                    horizontal = 3.dp
+                ),
+                thickness = 1.dp,
+                color = Color(0x80121212)
+            )
+            Text(
+                "안드로이드 위클리", style = SoakTheme.typography.body13.copy(
+                    color = Color(0x80121212)
+                )
+            )
+        }
+    }
+}
