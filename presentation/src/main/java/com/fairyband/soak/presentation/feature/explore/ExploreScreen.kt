c23dc4691f13e5eca892fae36dff28389b4b881a
HamBP <sjy_9484@naver.com>
2025-12-17 21:32:25 +0900

    [FEAT] explore 화면의 글씨 색깔과 간격을 조정했어요.

--- presentation/src/main/java/com/fairyband/soak/presentation/feature/explore/ExploreScreen.kt
+++ presentation/src/main/java/com/fairyband/soak/presentation/feature/explore/ExploreScreen.kt
@@ -3,10 +3,13 @@ package com.fairyband.soak.presentation.feature.explore
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.WindowInsets
+import androidx.compose.foundation.layout.asPaddingValues
 import androidx.compose.foundation.layout.height
-import androidx.compose.foundation.layout.navigationBarsPadding
+import androidx.compose.foundation.layout.navigationBars
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.grid.GridCells
 import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
@@ -18,13 +21,14 @@ import androidx.compose.runtime.getValue
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
 import com.fairyband.soak.core.theme.LocalSoakColors
-import com.fairyband.soak.core.theme.SoakColors
 import com.fairyband.soak.core.theme.SoakTheme
 import com.fairyband.soak.data.model.response.ExploreContentResponse
+import com.fairyband.soak.presentation.R
 import org.koin.androidx.compose.koinViewModel
 
 @Composable
@@ -45,18 +49,26 @@ fun ExploreScreen(viewModel: ExploreViewModel = koinViewModel()) {
     Column(
         modifier = Modifier.padding(horizontal = 16.dp)
     ) {
-        Text("전체 (${news.size})")
+        Text(
+            modifier = Modifier.padding(top = 8.dp),
+            text = stringResource(R.string.explore_count_of_articles, news.size),
+            style = SoakTheme.typography.body14.copy(
+                color = soakColors.textStrongInverse,
+                fontWeight = FontWeight.Bold
+            )
+        )
+
         LazyVerticalGrid(
             columns = GridCells.Fixed(2),
             verticalArrangement = Arrangement.spacedBy(8.dp),
             horizontalArrangement = Arrangement.spacedBy(8.dp),
+            contentPadding = PaddingValues(
+                top = 8.dp,
+                bottom = WindowInsets.navigationBars.asPaddingValues().calculateBottomPadding()
+            )
         ) {
             items(news.size) { index ->
                 Card(news[index], cardColors[index % 6])
-            }
-
-            item {
-                Spacer(modifier = Modifier.navigationBarsPadding())
             }
         }
     }
