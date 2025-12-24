b10b885f654b46c97134ecfcefa126bde88a858d
HamBP <sjy_9484@naver.com>
2025-12-24 09:11:57 +0900

    [FEAT] 마지막 페이지를 판단하는 로직을 추가했어요.

--- presentation/src/main/java/com/fairyband/soak/presentation/feature/explore/ExploreViewModel.kt
+++ presentation/src/main/java/com/fairyband/soak/presentation/feature/explore/ExploreViewModel.kt
@@ -16,14 +16,21 @@ class ExploreViewModel(
     private val _news = MutableStateFlow<List<ExploreFeed>>(emptyList())
     val news = _news.asStateFlow()
 
+    private var isLastPage = false
+
     suspend fun loadFeeds() {
-        // TODO: 마지막 페이지를 판단하는 기준이 필요하다. (개수가 나누어 떨어지지 않거나, 새로운 페이지의 크기가 0일 때)
+        if (isLastPage) return
+
         val pageSize = 20
         val nextPage = news.value.size / 20
 
         _news.update { existing ->
             val new = newsRepository.getExploreContents(page = nextPage, size = pageSize)
                 .map { it.toExploreFeed() }
+
+            if (new.size < pageSize) {
+                isLastPage = true
+            }
 
             existing + new
         }
