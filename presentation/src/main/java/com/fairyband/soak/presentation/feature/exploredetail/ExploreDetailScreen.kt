cd018ca8765c75793134cf78d015536e4dff1a47
HamBP <sjy_9484@naver.com>
2025-12-26 18:24:25 +0900

    [FEAT] 상세 페이지의 앱 바를 만들었어요.

--- presentation/src/main/java/com/fairyband/soak/presentation/feature/exploredetail/ExploreDetailScreen.kt
+++ presentation/src/main/java/com/fairyband/soak/presentation/feature/exploredetail/ExploreDetailScreen.kt
@@ -2,32 +2,62 @@ package com.fairyband.soak.presentation.feature.exploredetail
 
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Text
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.systemBarsPadding
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.blur
 import androidx.compose.ui.graphics.Brush
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.vectorResource
 import androidx.compose.ui.unit.dp
 import com.fairyband.soak.core.designsystem.systembar.DarkSystemBar
 import com.fairyband.soak.core.theme.SoakTheme
+import com.fairyband.soak.presentation.LocalNavController
 import com.fairyband.soak.presentation.R
 import com.fairyband.soak.presentation.model.ExploreFeed
 
 @Composable
 fun ExploreDetailScreen(index: Int, feeds: List<ExploreFeed>) {
+    val navController = LocalNavController.current
+
     DetailBackground()
 
-    Text("explore detail")
+    Row(
+        modifier = Modifier
+            .systemBarsPadding()
+            .height(48.dp)
+            .padding(horizontal = 8.dp),
+        horizontalArrangement = Arrangement.Start,
+        verticalAlignment = Alignment.Bottom,
+    ) {
+        IconButton(
+            onClick = {
+                navController.pop()
+            },
+        ) {
+            Icon(
+                modifier = Modifier.size(24.dp),
+                imageVector = ImageVector.vectorResource(R.drawable.ic_arrow_back),
+                contentDescription = null,
+                tint = SoakTheme.colors.iconStrongInverse,
+            )
+        }
+    }
 }
 
 @Composable
@@ -52,6 +82,7 @@ private fun DetailBackground() {
             modifier = Modifier
                 .fillMaxWidth()
                 .height(180.dp)
+                .blur(20.dp)
                 .background(
                     brush = Brush.verticalGradient(
                         colors = listOf(
@@ -60,7 +91,7 @@ private fun DetailBackground() {
                         )
                     )
                 )
-                .blur(20.dp)
+
         )
     }
 }
