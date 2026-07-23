3e3652411f57ab2be6fc90f328208a861b4fae24
algosketch <algosketch@gmail.com>
2026-07-23 21:06:54 +0900

    [FEAT] UserInfoResponse에 isOnboarded 프로퍼티 추가

--- data/src/main/java/com/fairyband/soak/data/model/response/UserInfoResponse.kt
+++ data/src/main/java/com/fairyband/soak/data/model/response/UserInfoResponse.kt
@@ -7,5 +7,5 @@ data class UserInfoResponse(
     val id: Long,
     val preferences: List<String> = emptyList(),
     val workingExperience: String? = null,
-    val isOnboarded: Boolean = false,
+    val isOnboarded: Boolean = false, // true일 경우 온보딩(맞춤 설정)을 해야 하는 사용자라는 의미
 )
